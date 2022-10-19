15bb6867077ee9a37da246084ba489475eba3cad
Grace <griseldacastaneda2013@gmail.com>
2022-10-18 20:27:09 -0700

    boolean, string dynamic js typing

--- javascript.js
+++ javascript.js
@@ -6,3 +6,13 @@ const ageJonas = now - 1991;
 const ageSarah = now - 2018;
 console.log(ageJonas, ageSarah);
 
+let javascriptIsFun= true;
+console.log(javascriptIsFun);
+
+console.log(typeof javascriptIsFun);
+
+
+javascriptIsFun = "Yes!"
+
+console.log(typeof javascriptIsFun);
+
