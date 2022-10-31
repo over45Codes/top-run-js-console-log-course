97e1e6ca45dfce830a0debf4c2416020021837df
Grace <griseldacastaneda2013@gmail.com>
2022-10-31 06:50:05 -0700

    commented out previous lessons to allow new ones

--- javascript.js
+++ javascript.js
@@ -1,53 +1,52 @@
-let helloWorld= "Hello, World!";
-console.log(helloWorld)
+//let helloWorld= "Hello, World!";
+//console.log(helloWorld)
 
-const now = 2037;
-const ageJonas = now - 1991;
-const ageSarah = now - 2018;
-console.log(ageJonas, ageSarah);
+//const now = 2037;
+//const ageJonas = now - 1991;
+//const ageSarah = now - 2018;
+//console.log(ageJonas, ageSarah);
 
-let javascriptIsFun= true;
-console.log(javascriptIsFun);
+//let javascriptIsFun= true;
+//console.log(javascriptIsFun);
 
-console.log(typeof javascriptIsFun);
-
-
-javascriptIsFun = "Yes!"
-
-console.log(typeof javascriptIsFun);
-
-const firstName = 'Jonas';
-const lastName = 'Peters';
-console.log(firstName + ' ' + lastName);
+//console.log(typeof javascriptIsFun);
 
 
-console.log(23 + 97);
+//javascriptIsFun = "Yes!"
+
+//console.log(typeof javascriptIsFun);
+
+//const firstName = 'Jonas';
+//const lastName = 'Peters';
+//console.log(firstName + ' ' + lastName);
+
+//console.log(23 + 97);
 
 //The two numbers can be literals
-let c = 100 + 50;
-console.log(c);
+//let c = 100 + 50;
+//console.log(c);
 
-let a = 100;
-let b = 50;
-let x = a + b;
-document.getElementById("demo").innerHTML = x;
+//let a = 100;
+//let b = 50;
+//let x = a + b;
+//document.getElementById("demo").innerHTML = x;
 
-let d = 3;
-let e = (100 + 50) * d;
-document.getElementById("demo1").innerHTML = e;
+//let d = 3;
+//let e = (100 + 50) * d;
+//document.getElementById("demo1").innerHTML = e;
 
-let f = 5;
-let y = 2;
-let z = f + y;
-document.getElementById("demo2").innerHTML = z;
+//let f = 5;
+//let y = 2;
+//let z = f + y;
+//document.getElementById("demo2").innerHTML = z;
 
-console.log((4 + 6 + 9) / 77)
+//console.log((4 + 6 + 9) / 77)
 
-let g = 1;
+//let g = 1;
 
-g = -g;
-alert( g ); // -1, unary negation
+//g = -g;
+//alert( g ); // -1, unary negation
 
-let k = 1, l = 3;
-alert( l - k ); //2, binary minus subtracts values
+//let k = 1, l = 3;
+//alert( l - k ); //2, binary minus subtracts values
 
