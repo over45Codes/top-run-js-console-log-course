3d275b25f94248b2c295bb2d582434978a3b165a
Grace <griseldacastaneda2013@gmail.com>
2022-10-31 06:54:26 -0700

    remainder operator

--- javascript.js
+++ javascript.js
@@ -50,3 +50,6 @@
 //let k = 1, l = 3;
 //alert( l - k ); //2, binary minus subtracts values
 
+//alert ( 5 % 2 ); // 1, a remainder of 5 divided by 2
+
+//alert( 8 % 3 ); // 2, a remainder of 8 divided by 3
