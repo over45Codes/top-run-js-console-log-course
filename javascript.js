0e3aec0b040f45345df6a35c08fc750aca755125
Grace <griseldacastaneda2013@gmail.com>
2022-11-01 06:06:22 -0700

    edited all the content and commented out

--- javascript.js
+++ javascript.js
@@ -1,55 +1,63 @@
-//let helloWorld= "Hello, World!";
-//console.log(helloWorld)
+// let helloWorld= "Hello, World!";
+// console.log(helloWorld)
 
-//const now = 2037;
-//const ageJonas = now - 1991;
-//const ageSarah = now - 2018;
-//console.log(ageJonas, ageSarah);
+// const now = 2037;
+// const ageJonas = now - 1991;
+// const ageSarah = now - 2018;
+// console.log(ageJonas, ageSarah);
 
-//let javascriptIsFun= true;
-//console.log(javascriptIsFun);
+// let javascriptIsFun= true;
+// console.log(javascriptIsFun);
 
-//console.log(typeof javascriptIsFun);
+// console.log(typeof javascriptIsFun);
 
 
-//javascriptIsFun = "Yes!"
+// javascriptIsFun = "Yes!"
 
-//console.log(typeof javascriptIsFun);
+// console.log(typeof javascriptIsFun);
 
-//const firstName = 'Jonas';
-//const lastName = 'Peters';
-//console.log(firstName + ' ' + lastName);
+// const firstName = 'Jonas';
+// const lastName = 'Peters';
+// console.log(firstName + ' ' + lastName);
 
-//console.log(23 + 97);
+// console.log(23 + 97);
 
-//The two numbers can be literals
-//let c = 100 + 50;
-//console.log(c);
+// The two numbers can be literals
+// let c = 100 + 50;
+// console.log(c);
 
-//let a = 100;
-//let b = 50;
-//let x = a + b;
-//document.getElementById("demo").innerHTML = x;
+// let a = 100;
+// let b = 50;
+// let x = a + b;
+// document.getElementById("demo").innerHTML = x;
 
-//let d = 3;
-//let e = (100 + 50) * d;
-//document.getElementById("demo1").innerHTML = e;
+// let d = 3;
+// let e = (100 + 50) * d;
+// document.getElementById("demo1").innerHTML = e;
 
-//let f = 5;
-//let y = 2;
-//let z = f + y;
-//document.getElementById("demo2").innerHTML = z;
+// let f = 5;
+// let y = 2;
+// let z = f + y;
+// document.getElementById("demo2").innerHTML = z;
 
-//console.log((4 + 6 + 9) / 77)
+// console.log((4 + 6 + 9) / 77)
 
-//let g = 1;
+// An operand – is what operators are applied to. For instance, in the multiplication of 5 * 2 there are two operands: the left operand is 5 and the right operand is 2. Sometimes, people call these “arguments” instead of “operands”.
 
-//g = -g;
-//alert( g ); // -1, unary negation
+// An operator is unary if it has a single operand. For example, the unary negation - reverses the sign of a number:
+// let g = 1;
+// g = -g;
+// alert( g ); // -1, unary negation
 
-//let k = 1, l = 3;
-//alert( l - k ); //2, binary minus subtracts values
+// let k = 1, l = 3;
+// alert( l - k ); //2, binary minus subtracts values
 
-//alert ( 5 % 2 ); // 1, a remainder of 5 divided by 2
+// alert ( 5 % 2 ); // 1, a remainder of 5 divided by 2
 
-//alert( 8 % 3 ); // 2, a remainder of 8 divided by 3
+// The remainder operator %, despite its appearance, is not related to percents.
+
+// The result of a % b is the remainder of the integer division of a by b.
+
+// For instance:
+
+// alert( 8 % 3 ); // 2, a remainder of 8 divided by 3
