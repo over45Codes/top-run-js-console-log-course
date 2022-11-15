8456a034e2600a9aa93f7c5c330c827d07691afe
Grace <griseldacastaneda2013@gmail.com>
2022-11-15 06:54:07 -0800

    top assignment part one exercises

--- javascript.js
+++ javascript.js
@@ -13,7 +13,6 @@
 
 // console.log(typeof javascriptIsFun);
 
-
 // javascriptIsFun = "Yes!"
 
 // console.log(typeof javascriptIsFun);
@@ -21,8 +20,6 @@
 // const firstName = 'Jonas';
 // const lastName = 'Peters';
 // console.log(firstName + ' ' + lastName);
-
-// console.log(23 + 97);
 
 // The two numbers can be literals
 // let c = 100 + 50;
@@ -42,9 +39,9 @@
 // let z = f + y;
 // document.getElementById("demo2").innerHTML = z;
 
-// console.log((4 + 6 + 9) / 77)
+// Basic operators, math by Javascript.info/operator webpage / See below: 
 
-// An operand – is what operators are applied to. For instance, in the multiplication of 5 * 2 there are two operands: the left operand is 5 and the right operand is 2. Sometimes, people call these “arguments” instead of “operands”.
+//An operand – is what operators are applied to. For instance, in the multiplication of 5 * 2 there are two operands: the left operand is 5 and the right operand is 2. Sometimes, people call these “arguments” instead of “operands”.
 
 // An operator is unary if it has a single operand. For example, the unary negation - reverses the sign of a number:
 // let g = 1;
@@ -60,9 +57,9 @@
 
 // For instance:
 
-// alert ( 5 % 2 ); // 1, a remainder of 5 divided by 2
+// alert ( 5 % 2 ); // 1, a remainder of 5 divided by 2;
 
-// alert( 8 % 3 ); // 2, a remainder of 8 divided by 3
+// alert( 8 % 3 ); // 2, a remainder of 8 divided by 3;
 
 // Exponentiation**
  // The exponentiation operator a ** b raises a to the power of b.
@@ -71,4 +68,44 @@
 
 // For instance:
 
+
+// TOP Assignment // Fundamentals Part 1 / Foundations Course 
+// Try the following exercises (and don’t forget to use console.log()!):
+
+// Add 2 numbers together! (just type console.log(23 + 97) into your html file)
+//  console.log(23 + 97);
+
+//Add a sequence of 6 different numbers together.
+// console.log(4 + 5 + 6 + 4 + 5 + 6);
+
+// Print the solution to the following equation: (4 + 6 + 9) / 77
+//   console.log((4 + 6 + 9) / 77);
+//    Answer should be approximately 0.24675
+
+// Let’s use variables!
+
+// Type this statement at the top of the script tag: let a = 10
+// in the console console.log(a) should print 10
+// let a = 10;
+// console.log(a);
+
+// Try the following in the console: 9 * a
+//  console.log(9 * a);
+// and this: let b = 7 * a (returns undefined *) and then console.log(b)
+//  let b = 7 * a;
+//  console.log(b);
+// note: * As you might have noticed by running Javascript code in the console, the console prints the result of the code it executes (called a return statement). You will learn more about these in the next lessons, however for now it is good to remember that a declaration with an assignment (such as let b = 7 * a) returns undefined and so you cannot declare and assign a value to a variable and read its value in the same line. = Done in the console.
+
+// You should be getting the hang of this by now… try this sequence:
+
+// Declare a constant variable max with the value 57
+//  const max = 57;
+// Set another variable actual to max - 13
+//   let actual = max - 13;
+// Set another variable percentage to actual / max
+//   let percentage = actual / max;
+// If you type percentage in the console and press enter you should see a value like 0.7719
+//    typed percentage in the console and I got 0.7719298245614035
+
+// Done!!!!! 11/15/2022 
 
