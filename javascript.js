5bbe916ed6148ea0a6107d72279042d9c882b452
Grace <griseldacastaneda2013@gmail.com>
2022-11-16 07:02:11 -0800

    basic operators exercises

--- javascript.js
+++ javascript.js
@@ -39,7 +39,7 @@
 // let z = f + y;
 // document.getElementById("demo2").innerHTML = z;
 
-// Basic operators, math by Javascript.info/operator webpage / See below: 
+// Basic operators, math by Javascript.info/operator webpage / https://javascript.info/operators#numeric-conversion-unary See below: 
 
 //An operand – is what operators are applied to. For instance, in the multiplication of 5 * 2 there are two operands: the left operand is 5 and the right operand is 2. Sometimes, people call these “arguments” instead of “operands”.
 
@@ -67,6 +67,55 @@
 // In school maths, we write that as ab.
 
 // For instance:
+
+// alert ( 2 ** 2 ); // 2² = 4
+// alert ( 2 ** 3 ); // 2³ = 8
+// alert ( 2 ** 4 ); // 2⁴ = 16
+
+// Just like in maths, the exponentiation operator is defined for non-integer numbers as well.
+
+// For example, a square root is an exponentiation by ½:
+
+// alert ( 4 ** (1/2) ); // 2 (power of 1/2 is the same as a square root)
+// alert ( 8 ** (1/3) ); // 2 (power of 1/3 is the same as a cubic root)
+
+// String concatenation with binary +
+// Let’s meet the features of JavaScript operators that are beyond school arithmetics.
+
+// Usually, the plus operator + sums numbers.
+
+// But, if the binary + is applied to strings, it merges (concatenates) them:
+
+// let s = "my" + "string";
+// alert(s); // mystring
+
+// Note that if any of the operands is a string, then the other one is converted to a string too.
+
+// For example:
+
+// alert( '1' + 2 ); // "12"
+// alert( 2 + '1' ); // "21"
+
+// See, it doesn’t matter whether the first operand is a string or the second one.
+
+// Here’s a more complex example:
+
+ // alert( 2 + 2 + '1' ); // "41 and not "221"
+
+//  Here, operators work one after another. The first + sums two numbers, so it returns 4, then the next + adds the string 1 to it, so it’s like 4 + '1' = '41'.
+
+// alert('1' + 2 + 2); // "122 and not "14"
+
+// Here, the first operand is a string, the compiler treats the other two operands as strings too. The 2 gets concatenated to '1', so it’s like '1' + 2 = "12" and "12" + 2 = "122".
+
+// The binary + is the only operator that supports strings in such a way. Other arithmetic operators work only with numbers and always convert their operands to numbers.
+
+// Here’s the demo for subtraction and division:
+
+ // alert( 6 - '2' ); // 4, converts '2' to a number 
+// alert( '6' / '2' ); // 3, converts both operands to numbers
+
+// ****** left off here on 11/16/2022 *****
 
 
 // TOP Assignment // Fundamentals Part 1 / Foundations Course 
@@ -115,5 +164,7 @@
 // name = "John";
 // admin = name;
 // alert( admin );
+
+
 
 
